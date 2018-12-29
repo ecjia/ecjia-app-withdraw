90c20034f0ce5faaf2256e41372b8cc9ff3855b4
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-12-29 15:41:53 +0800

    获取提现类型

---
+++ classes/WithdrawBankType.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: royalwang
+ * Date: 2018/12/29
+ * Time: 14:55
+ */
+
+namespace Ecjia\App\Withdraw;
+
+
+class WithdrawBankType
+{
+
+    protected $plugins;
+
+    public function __construct()
+    {
+        $WithdrawPlugin = new WithdrawPlugin();
+
+        $this->plugins = $WithdrawPlugin->getAvailablePlugins();
+
+    }
+
+
+    public function getDisplayBankType()
+    {
+        $support_bank = false;
+
+        $plugins = $this->plugins->map(function($model) {
+            $plugin = $model->channel($model->withdraw_code);
+            $bank_type = $plugin->getBankType();
+
+            $data = [
+                'bank_name' => $plugin->getDisplayName(),
+                'bank_type' => $bank_type,
+            ];
+
+            return $data;
+        })->filter(function ($item) use (& $support_bank) {
+            if ($item['bank_type'] == 'bank') {
+                $support_bank = true;
+                return false;
+            }
+            return true;
+        });
+
+        if ($support_bank) {
+            $plugins->push([
+                'bank_name' => '银行转帐',
+                'bank_type' => 'bank',
+            ]);
+        }
+
+        return $plugins->all();
+    }
+
+}
