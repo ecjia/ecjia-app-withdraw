46b8e2e2c7e57cf47d241d5c3fbbedaa0c8545bc
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-12-29 14:20:53 +0800

    添加数据模型

---
+++ classes/Models/WithdrawUserBankModel.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Ecjia\App\Withdraw\Models;
+
+use Royalcms\Component\Database\Eloquent\Model;
+
+class WithdrawUserBankModel extends Model
+{
+    protected $table = 'withdraw_user_bank';
+    
+    protected $primaryKey = 'id';
+    
+    /**
+     * 可以被批量赋值的属性。
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'bank_name',
+        'bank_card',
+        'bank_branch_name',
+        'cardholder',
+        'bank_en_short',
+        'user_id',
+        'user_type',
+        'bank_type',
+    ];
+    
+    /**
+     * 该模型是否被自动维护时间戳
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+    
+}
